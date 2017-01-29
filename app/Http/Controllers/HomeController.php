981a0a7b33f3b44fac3ff040070c022a58c86096
Oliver Carlos <oliverpascualcarlos@gmail.com>
2017-01-29 16:25:48 +0800

    moved terms and agreement function to HomeController.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,9 @@ use App\Http\Requests;
 use App\terminal as Terminal;
 use App\utilities_company as Utilities;
 use App\test_image as Image;
+use App\reserve_cancellation_percentage as Percentage;
+use App\days_span_to_reserve as ReservationDay;
+use App\reservation_days_to_void as Void;
 
 class HomeController extends Controller
 {
@@ -40,5 +43,27 @@ class HomeController extends Controller
     						 ->get();
     	$no_date = true;
     	return view('welcome', compact('terminals', 'no_date'));
+    }
+
+    public function termsAndAgreement()
+    {
+        $percentages = Percentage::select('ReserveCancellationPercentage_NumberOfDays', 'ReserveCancellationPercentage_PercentageReturn')
+                              ->get();
+        $totalDays = Percentage::count();
+        $days = ReservationDay::select('DaysSpanToReserve_Days')->orderBy('DaysSpanToReserve_Id', 'desc')->take(1)->get();
+        $voidDay = Void::select('ReservationDaysToVoid_Days')->orderBy('ReservationDaysToVoid_Id', 'desc')->take(1)->get();
+        if (!$days->count() || !$voidDay->count())
+        {
+            $days = 0;
+            $voidDay = 0;
+            return view('pages.purchase.ta', ['title' => 'Terms And Agreement - Bus Reservation And Ticketing System', 'percentages' => $percentages,
+                                          'totalDays' => $totalDays, 'days' => $days, 'voidDay' => $voidDay]);
+        }
+    return view('pages.purchase.ta', [
+        'title' => 'Terms And Agreement - Bus Reservation And Ticketing System', 'percentages' => $percentages,
+        'totalDays' => $totalDays, 
+        'days' => $days[0]->DaysSpanToReserve_Days , 
+        'voidDays' => $voidDay[0]->ReservationDaysToVoid_Days
+        ]);
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,10 +10,6 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-use App\reserve_cancellation_percentage as Percentage;
-use App\days_span_to_reserve as ReservationDay;
-use App\reservation_days_to_void as Void;
 Route::get('/', 'HomeController@index');
 Route::get('/about', function () {
 		return view('about');
@@ -22,23 +18,7 @@ Route::get('/noschedule', 'HomeController@fail');
 Route::post('/travel_schedules', 'ScheduleController@show');
 Route::get('/routes', 'RouteController@index');
 Route::get('/route/{route}', 'ScheduleController@route_show');
-Route::get('/bus/passengers/terms_and_agreement', function () {
-	$percentages = Percentage::select('ReserveCancellationPercentage_NumberOfDays', 'ReserveCancellationPercentage_PercentageReturn')
-							  ->get();
-	$totalDays = Percentage::count();
-	$days = ReservationDay::select('DaysSpanToReserve_Days')->orderBy('DaysSpanToReserve_Id', 'desc')->take(1)->get();
-	$voidDay = Void::select('ReservationDaysToVoid_Days')->orderBy('ReservationDaysToVoid_Id', 'desc')->take(1)->get();
-	if (!$days->count() || !$voidDay->count())
-	{
-		$days = 0;
-		$voidDay = 0;
-		return view('pages.purchase.ta', ['title' => 'Terms And Agreement - Bus Reservation And Ticketing System', 'percentages' => $percentages,
-									  'totalDays' => $totalDays, 'days' => $days, 'voidDay' => $voidDay]);
-	}
-	return view('pages.purchase.ta', ['title' => 'Terms And Agreement - Bus Reservation And Ticketing System', 'percentages' => $percentages,
-									  'totalDays' => $totalDays, 'days' => $days[0]->DaysSpanToReserve_Days , 
-									  'voidDays' => $voidDay[0]->ReservationDaysToVoid_Days]);
-});
+Route::get('/bus/passengers/terms_and_agreement', 'HomeController@termsAndAgreement');
 Route::post('/book_seats', 'TransactionController@input');
 Route::post('/book_seats/iterate', 'TransactionController@view');
 Route::post('/transaction/complete', 'TransactionController@store');
